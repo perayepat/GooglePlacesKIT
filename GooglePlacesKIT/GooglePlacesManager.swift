03279e9539148c94a540fddf1469d82561f58769
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-04-08 15:09:56 +0200

    Finding the places using the google services

--- GooglePlacesKIT/GooglePlacesManager.swift
+++ GooglePlacesKIT/GooglePlacesManager.swift
@@ -1,6 +1,15 @@
 import Foundation
 import GooglePlaces
 
+struct Place{
+    let name: String
+    let id: String
+}
+
+enum PlacesErrors: Error{
+    case failedToFind
+}
+
 final class GooglePlacesManager{
     static let shared = GooglePlacesManager()
     
@@ -8,7 +17,27 @@ final class GooglePlacesManager{
     
     private init (){}
     
+    
+    
     public func setUp(){
         GMSPlacesClient.provideAPIKey("AIzaSyCcyVr8-75zDXC6H9OFeDRj4XS5aP3182k")
+    }
+    
+    public func findPlaces(query: String, completion: @escaping (Result<[Place],Error>) -> Void){
+        let filter = GMSAutocompleteFilter()
+        filter.types = ["geocode"]
+        client.findAutocompletePredictions(fromQuery: query, filter: filter, sessionToken: nil) { results, error in
+            guard let results = results, error == nil else {
+                completion(.failure(PlacesErrors.failedToFind))
+                return
+            }
+            
+            let places : [Place] = results.compactMap {
+                Place(name: $0.attributedFullText.string,
+                      id: $0.placeID)
+            }
+            
+            completion(.success(places))
+        }
     }
 }
---
+++ PlaceModel.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Place{
+    let name: String
+    let id: String
+}
