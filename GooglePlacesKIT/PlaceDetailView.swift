9e411e11e3aba632573eea7a6ca3558ab34ad862
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-04-09 14:58:09 +0200

    Place detail view being added

--- GooglePlacesKIT/PlaceDetailView.swift
+++ GooglePlacesKIT/PlaceDetailView.swift
@@ -2,12 +2,34 @@ import SwiftUI
 
 struct PlaceDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            placeHero
+        }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .leading)
+        .padding()
     }
 }
 
 struct PlaceDetailView_Previews: PreviewProvider {
     static var previews: some View {
         PlaceDetailView()
+    }
+}
+
+
+//Components
+extension PlaceDetailView{
+    var placeHero: some View{
+        VStack(alignment: .leading, spacing: 15){
+            Text("Place Name")
+                .font(.title)
+                .bold()
+            Text("place description")
+            HStack {
+                Image(systemName: "location.fill.viewfinder")
+                    .font(.title2)
+                Text("PlaceCode")
+            }
+        }
     }
 }
